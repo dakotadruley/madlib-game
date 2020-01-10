1f44caef1eb4c6ab9ef29d4c279e6e0dd222e94d
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-01-09 21:06:26 -0800

    2nd Draft

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 
+function fillInBlanks(){
 
 const AnimalInput = document.getElementById('Animal');
-const AnimalOutput = AnimalInput.nodeValue;
+const AnimalOutput = AnimalInput.Value;
 AnimalInput.textContent = AnimalOutput; 
+
+const FlowerInput = document.getElementById('Flower');
+const FlowerOutput = FlowerInput.Value;
+FlowerInput.textContent = FlowerOutput; 
+
+const ObjectInput = document.getElementById('Object');
+const ObjectOutput = ObjectInput.Value;
+ObjectInput.textContent = ObjectOutput; 
+
+const VerbInput = document.getElementById('Verb');
+const VerbOutput = VerbInput.Value;
+VerbInput.textContent = VerbOutput; 
+
+const HouseholdItemInput = document.getElementById('Household Item');
+const HouseholdItemOutput = HouseholdItem.Value;
+HouseholdItem.textContent = HouseholdItemOutput; 
+
+const AgeInput = document.getElementById('Age');
+const AgeOutput = AnimalInput.Value;
+AgeInput.textContent = AgeOutput; 
+
+document.getElementById('Results').style.visibility = "visible";
+
+};
